39caa0d94617618f66f921b5ef773b72dd166751
Sai-2002 <smnjr2002@gmail.com>
2022-11-12 22:13:03 +0530

    Redis implementation on list_actions

--- routes/list_actions.py
+++ routes/list_actions.py
@@ -1,5 +1,5 @@
-import mimetypes
-from routes import app, version
+import ast
+from routes import app, version, redis_cli
 import json
 from flask import Response, request, jsonify
 import sqlite3
@@ -28,6 +28,10 @@ def createlist(u_id):
         list_id = c.fetchone()
         c.execute("INSERT INTO creates(userId, listId) VALUES(?,?)",(u_id[0],list_id[0]))
         conn.commit()
+        redis_cli.rpush(u_id[0], list_id[0])
+        list_detatils["listId"] = list_id[0]
+        redis_cli.set(f"list{list_id[0]}", json.dumps(list_detatils))
+        print(list_detatils)
         conn.close()
 
         return Response(
@@ -55,6 +59,7 @@ def updatelisttitle(u_id, listId):
     }
 
     try:
+
         # creating database connection and cursor
         conn = sqlite3.connect(database_locale)
         c = conn.cursor()
@@ -63,6 +68,8 @@ def updatelisttitle(u_id, listId):
 
         if userId == u_id:
             c.execute("UPDATE list SET listName = ?, listDescription = ?, imageName = ? WHERE listId = ?", (list_details["listName"], list_details["listDescription"], list_details["imageName"], listId))
+            list_details["listId"] = listId
+            redis_cli.set(f"list{listId}", json.dumps(list_details))
         else:
             return Response(
                 response=json.dumps({"message": "Unauthorized access"}),
@@ -88,60 +95,83 @@ def updatelisttitle(u_id, listId):
 @app.route(f"{version}/getAllList", methods=["GET"])
 @token_required
 def getAllList(u_id):
+
+    resultArr = []
     try:
+        print("*********")
+        if redis_cli.exists(u_id[0]):
+            print("hell")
+            lists = redis_cli.lrange(u_id[0], 0, -1)
+            for List in lists:
+                resultArr.append(ast.literal_eval(redis_cli.get(f"list{List}")))
+        else: 
+            print("hel")
+            conn = sqlite3.connect(database_locale)
+            c = conn.cursor()
+            c.execute("select * from list where listId in (select c.listId from list l, creates c where l.listId = c.listId and c.userId = ?)", (u_id[0],))
+            lists = c.fetchall()
 
-        conn = sqlite3.connect(database_locale)
-        c = conn.cursor()
-        c.execute("select * from list where listId in (select c.listId from list l, creates c where l.listId = c.listId and c.userId = ?)", (u_id[0],))
-        lists = c.fetchall()
-        resultArr = []
-
-        for lis in lists:
-            if len(list_profile) == len(lis):
-                resultDict = {list_profile[i] : lis[i] for i, _ in enumerate(list_profile)}
-                resultArr.append(resultDict)
-            
-        conn.commit()
-        conn.close()
+            for lis in lists:
+                if len(list_profile) == len(lis):
+                    resultDict = {list_profile[i] : lis[i] for i, _ in enumerate(list_profile)}
+                    redis_cli.rpush(u_id[0],lis[0])
+                    redis_cli.set(f"list{lis[0]}", json.dumps(resultDict))
+                    resultArr.append(resultDict)
+                
+            conn.commit()
+            conn.close()
         return Response(
             response=json.dumps({"Lists": resultArr}),
             status=200,
             mimetype="application/json"
         )
     except:
-        return Response(
-            response=json.dumps({"message":"Cannot display all list"}),
-            status=200,
-            mimetype="application/json"
-        )
+            return Response(
+                response=json.dumps({"message":"Cannot display all list"}),
+                status=200,
+                mimetype="application/json"
+            )
 
 
 @app.route(f"{version}/getList/<listId>", methods=["GET"])
 @token_required
 def getList(u_id, listId):
+        
     try:
-        conn = sqlite3.connect(database_locale)
-        c = conn.cursor()
-        c.execute("SELECT userId FROM creates WHERE listId=?",(listId,))
-        userId = c.fetchone()
-
-        if userId == u_id:
-            c.execute("SELECT * FROM list WHERE listId = ?",(listId,))
-            List = c.fetchone()
-            if len(list_profile) == len(List):
-                resultDict = {list_profile[i] : List[i] for i, _ in enumerate(list_profile)}
-        else:
+        print("cache Hit")
+        if redis_cli.exists(u_id[0]):
+            List = ast.literal_eval(redis_cli.get(f"list{listId}"))
             return Response(
-                response=json.dumps({"message": "Unauthorized access"}),
+                response=json.dumps({"message": List}),
                 status=200,
                 mimetype="application/json"
             )
+        else:
+            conn = sqlite3.connect(database_locale)
+            c = conn.cursor()
+            c.execute("SELECT userId FROM creates WHERE listId=?",(listId,))
+            userId = c.fetchone()
 
-        conn.commit()
-        conn.close()
+            if userId == u_id:
+                c.execute("SELECT * FROM list WHERE listId = ?",(listId,))
+                List = c.fetchone()
+                if len(list_profile) == len(List):
+                    resultDict = {list_profile[i] : List[i] for i, _ in enumerate(list_profile)}
+                    redis_cli.rpush(u_id[0],List[0])
+                    redis_cli.set(f"list{List[0]}", json.dumps(resultDict))
+                    
+            else:
+                return Response(
+                    response=json.dumps({"message": "Unauthorized access"}),
+                    status=200,
+                    mimetype="application/json"
+                )
 
-        return Response(
-            response=json.dumps({"message": List}),
+            conn.commit()
+            conn.close()
+
+            return Response(
+            response=json.dumps({"message": resultDict}),
             status=200,
             mimetype="application/json"
         )
@@ -155,7 +185,7 @@ def getList(u_id, listId):
 
 @app.route(f"{version}/deleteList/<listId>", methods=["POST"])
 @token_required
-def deleteList(u_id, listId):
+def deleteList(u_id, listId):   
     try:
         conn = sqlite3.connect(database_locale)
         c = conn.cursor()
@@ -167,6 +197,18 @@ def deleteList(u_id, listId):
             List = c.fetchone()
             c.execute("DELETE FROM LIST WHERE listId = ?",(listId,))
             c.execute("DELETE FROM CREATES WHERE listId = ?",(listId,))
+            conn.commit()
+            c.execute("select * from list where listId in (select c.listId from list l, creates c where l.listId = c.listId and c.userId = ?)", (u_id[0],))
+            lists = c.fetchall()
+            
+            redis_cli.delete(u_id[0])
+            redis_cli.delete(f"list{listId}")
+            print(lists)
+
+            for lis in lists:
+                if len(list_profile) == len(lis):
+                    resultDict = {list_profile[i] : lis[i] for i, _ in enumerate(list_profile)}
+                    redis_cli.rpush(u_id[0],lis[0])
         else:
             return Response(
                 response=json.dumps({"message": "Unauthorized access"}),
