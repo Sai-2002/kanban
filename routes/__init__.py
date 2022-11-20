2b1322e788d40baef8d443126d77030c4d5f146d
Sai-2002 <smnjr2002@gmail.com>
2022-11-20 10:00:11 +0530

    Added init py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'testsecretkey'
 version = "/api/v1"
+
+redis_cli = redis.Redis(host="localhost", port=6379, password="", decode_responses=True)
+
 
 from routes import user_action
 from routes import home
